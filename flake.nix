71bcf9a514d3d2ac2182053fbe4fa9e1fc5d8c33
spottyunderdog <spottyunderdog18@gmail.com>
2026-08-30 13:11:16 -0400

    changed home-manger and nixpkgs url to use the unstable releases

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
 
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
 
@@ -20,7 +20,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
